206d78e3a8359ea5ae920991913e0e7032079ba0
el-mehdi-ait-fakir <m.aitfakir.hi@gmail.com>
2020-04-20 20:53:58 +0200

    integrate Template

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<div class="lime-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <span class="footer-text">2019 © stacks</span>
+            </div>
+        </div>
+    </div>
+</div>
